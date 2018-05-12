4643665caa350797fb2d71652ea6f0e0b3126fd0
alephNaN <automatwon@gmail.com>
2018-05-12 03:50:47 -0700

    Componentize left nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,13 +1,67 @@
 import Link from 'next/link'
+import Anchor from 'grommet/components/Anchor'
+import IconDashboard from 'grommet/components/icons/base/Article'
+import IconActivity from 'grommet/components/icons/base/History'
+import IconBilling from 'grommet/components/icons/base/CreditCard'
+import IconDragDrop from 'grommet/components/icons/base/Select'
+import IconPin from 'grommet/components/icons/base/Pin'
+import IconGroup from 'grommet/components/icons/base/Group'
+import IconStart from 'grommet/components/icons/base/Directions'
+import Menu from 'grommet/components/Menu'
+import Anchor from 'grommet/components/Anchor'
+import Link from 'next/link'
+import Label from 'grommet/components/Label'
+import Animate from 'grommet/components/Animate'
+import Paragraph from 'grommet/components/Paragraph'
+import Sidebar from 'grommet/components/Sidebar'
+import Box from 'grommet/components/Box'
 
-/** Deprecated */
 export default () => (
-  <div>
-    <Link href="/">
-      <a>Home</a>
-    </Link>
-    <Link href="/version">
-      <a>Version Page</a>
-    </Link>
-  </div>
+  <Animate enter={{ "animation": "slide-left", "duration": 1000, "delay": 300 }}>
+    <Sidebar full={false}>
+      <Box flex='grow'
+        justify='start'>
+        <Menu size="large"
+          padding='none'>
+          <Link href="/">
+            <Anchor
+              icon={<IconDashboard />}
+              label='Overview'>
+            </Anchor>
+          </Link>
+          <Anchor href='#'
+            icon={<IconActivity />}
+            label='Activity'>
+          </Anchor>
+          <Anchor href='#'
+            icon={<IconActivity />}
+            label='APIs'>
+          </Anchor>
+          <Anchor href='#'
+            icon={<IconGroup />}
+            label='Admin'>
+          </Anchor>
+          <Anchor href='#'
+            icon={<IconBilling />}
+            label='Billing'>
+          </Anchor>
+          <Anchor href='#'
+            icon={<IconStart />}
+            label='Start'>
+          </Anchor>
+        </Menu>
+      </Box>
+      <Box>
+        <Label uppercase='true' id='pinboard'>
+          <IconPin /> Pinned
+        </Label>
+        <Box pad='medium'>
+          <Box id='pin-box' textAlign='center'>
+            <Paragraph>Drag a product here</Paragraph>
+            <IconDragDrop style={{ display: 'block', margin: 'auto' }} />
+          </Box>
+        </Box>
+      </Box>
+    </Sidebar>
+  </Animate>
 )
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,39 +1,22 @@
-import Nav from 'src/components/nav'
-import Header from 'src/components/header'
-import ProductSlider from 'src/components/product_slider'
-
 import App, { Container } from 'next/app'
-import React from 'react'
 import nextReduxWrapper from 'next-redux-wrapper'
-import makeStore from 'src/store/store'
+import React from 'react'
+import { Provider } from 'react-redux'
+
 import GrommetApp from 'grommet/components/App'
 import Split from 'grommet/components/Split'
 import Box from 'grommet/components/Box'
-import IconDashboard from 'grommet/components/icons/base/Article'
-import IconActivity from 'grommet/components/icons/base/History'
-import IconBilling from 'grommet/components/icons/base/CreditCard'
-import IconDragDrop from 'grommet/components/icons/base/Select'
-import IconPin from 'grommet/components/icons/base/Pin'
-import IconGroup from 'grommet/components/icons/base/Group'
-import IconStart from 'grommet/components/icons/base/Directions'
-import Menu from 'grommet/components/Menu'
-import Anchor from 'grommet/components/Anchor'
-import Link from 'next/link'
-import Label from 'grommet/components/Label'
-import Animate from 'grommet/components/Animate'
-import Paragraph from 'grommet/components/Paragraph'
-import Sidebar from 'grommet/components/Sidebar';
-import { Provider } from 'react-redux'
 
-
+import Nav from 'src/components/nav'
+import Header from 'src/components/header'
+import ProductSlider from 'src/components/product_slider'
+import makeStore from 'src/store/store'
 
 const pageViewStyle = {
   borderRadius: '20px',
   minHeight: '500px',
   backgroundColor: '#fafafa',
 }
-
-
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
@@ -56,53 +39,7 @@ class MyApp extends App {
           <Header />
           <ProductSlider />
           <Split fixed={false} flex='right' showOnResponsive='both' >
-            <Animate enter={{ "animation": "slide-left", "duration": 1000, "delay": 300 }}>
-              <Sidebar full={false}>
-                <Box flex='grow'
-                  justify='start'>
-                  <Menu size="large"
-                    padding='none'>
-                    <Link href="/">
-                      <Anchor
-                        icon={<IconDashboard />}
-                        label='Overview'>
-                      </Anchor>
-                    </Link>
-                    <Anchor href='#'
-                      icon={<IconActivity />}
-                      label='Activity'>
-                    </Anchor>
-                    <Anchor href='#'
-                      icon={<IconActivity />}
-                      label='APIs'>
-                    </Anchor>
-                    <Anchor href='#'
-                      icon={<IconGroup />}
-                      label='Admin'>
-                    </Anchor>
-                    <Anchor href='#'
-                      icon={<IconBilling />}
-                      label='Billing'>
-                    </Anchor>
-                    <Anchor href='#'
-                      icon={<IconStart />}
-                      label='Start'>
-                    </Anchor>
-                  </Menu>
-                </Box>
-                <Box>
-                  <Label uppercase='true' id='pinboard'>
-                    <IconPin /> Pinned
-                  </Label>
-                  <Box pad='medium'>
-                    <Box id='pin-box' textAlign='center'>
-                      <Paragraph>Drag a product here</Paragraph>
-                      <IconDragDrop style={{ display: 'block', margin: 'auto' }} />
-                    </Box>
-                  </Box>
-                </Box>
-              </Sidebar>
-            </Animate>
+            <Nav />
             <Box style={pageViewStyle}
               pad='medium'>
               <Provider store={store}>
